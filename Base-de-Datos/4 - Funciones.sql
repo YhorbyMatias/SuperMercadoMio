2ba05462feca4d07b2fa9dcb6a3f7a0b27c2fe34
christiancata <christiancata@gmail.com>
2015-09-07 17:42:14 -0400

    Update 4 - Funciones.sql

--- Base-de-Datos/4 - Funciones.sql
+++ Base-de-Datos/4 - Funciones.sql
@@ -1,1 +1,20 @@
+Create Function buscarUsuarios()
+Returns Table
+Return
+(
+	Select ROW_NUMBER() OVER (ORDER BY Nombres) As Numero,  Id_Usuario,
+	(Nombres + ' ' + Apellido_Paterno + ' ' + ISNULL(Apellido_Materno, '')) As Usuario
+	From Usuario 
+	Where Estado = 1
+)
+Go
 
+Create Function buscarProveedores()
+Returns Table
+Return
+(
+	Select ROW_NUMBER() OVER (ORDER BY Nombre) As Numero, Id_Proveedor, Nombre, Persona_De_Contacto, Direccion, Telefono, Celular, Numero_De_Cuenta
+	From Proveedor
+	Where Estado = 1
+)
+Go
