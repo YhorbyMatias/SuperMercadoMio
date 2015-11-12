e136bd449e42309e874e9181677dc174b0feb342
christiancata <christiancata@gmail.com>
2015-11-12 19:35:55 -0400

    Caja

---
+++ Ent/CajaEnt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ent
+{
+    public class CajaEnt
+    {
+        #region Atributos
+        private int id;
+        private string nombre_de_equipo;
+        private string numero;
+        private string estado;
+        #endregion
+        #region Propiedades
+        public int ID
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+        public string NOMBRE_DE_EQUIPO
+        {
+            get { return this.nombre_de_equipo; }
+            set { this.nombre_de_equipo = value; }
+        }
+        public string NUMERO
+        {
+            get { return this.numero; }
+            set { this.numero = value; }
+        }
+        public string ESTADO
+        {
+            get { return this.estado; }
+            set { this.estado = value; }
+        }
+        #endregion
+    }
+}
